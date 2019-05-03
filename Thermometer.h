67e1b145dc2a17a78d07a45a92c041d828f6a999
alexalmansa <alexalmansa5@gmail.com>
2019-05-03 19:12:22 +0200

    Add files via upload

---
+++ Thermometer.h
@@ -0,0 +1,26 @@
+/* 
+ * File:   
+ * Author: 
+ * Comments:
+ * Revision history: 
+ */
+
+// This is a guard condition so that contents of this file are not included
+// more than once.  
+#ifndef Thermometer
+#define	Thermometer
+
+#include <xc.h> // include processor files - each processor file is guarded.  
+#define T LATBbits.LATB13
+#define SERIE LATBbits.LATB14
+
+#define INOUT TRISBbits.TRISB14
+
+void MotorThermometer(void);
+void llegeixBit();
+
+
+
+
+
+#endif	
