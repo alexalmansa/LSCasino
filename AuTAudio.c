9299a58e81e7ed9b0fecdc89d6a34e8c2ce727e2
Marc Llort <marc.llort@students.salle.url.edu>
2019-05-08 10:32:07 +0200

    Update AuTAudio.c

--- AuTAudio.c
+++ AuTAudio.c
@@ -90,7 +90,8 @@ void MotorControlAudio(){
     switch(estatt){
         case 0:
             vanalog=AdGetMostra();
-            tempsConvertit=(1000*vanalog)/255;
+             
+            tempsConvertit=(1000*binaryToDecimal(vanalog))/1024;
             if (TiGetTics(timerTemps) >= tempsConvertit){
                 if(melodia==0){
                     seguentFrequencia();
@@ -144,3 +145,24 @@ void audioFinal(){
     changeAudioStatus();
     frequencia=0;
 }
+
+int binaryToDecimal(int n) 
+{ 
+    int num = n; 
+    int dec_value = 0; 
+  
+    // Initializing base value to 1, i.e 2^0 
+    int basee = 1; 
+  
+    int temp = num; 
+    while (temp) { 
+        int last_digit = temp % 10; 
+        temp = temp / 10; 
+  
+        dec_value += last_digit * basee; 
+  
+        basee = basee * 2; 
+    } 
+  
+    return dec_value; 
+} 
