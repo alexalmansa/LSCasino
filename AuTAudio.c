53493c7cbb5775e0db522e1ec3579db772fc49aa
Marc Llort <marc.llort@students.salle.url.edu>
2019-05-08 10:49:02 +0200

    Update AuTAudio.c

--- AuTAudio.c
+++ AuTAudio.c
@@ -1,3 +1,151 @@
+#include "AuTAudio.h"
+#include "AdTADC.h"
+
+#include <xc.h>
+
+static unsigned char timerAudio,timerTemps, estat,periode[MAX_PERIODES]={2,3,4,5},frequencia;
+
+void AuInit(){
+    SET_AUDIO_DIR();
+    AUDIO_ON();
+    timerAudio = TiGetTimer();
+    timerTemps = TiGetTimer();
+    estat = 2;
+    frequencia = 0;
+}
+
+unsigned char* getAudioPeriode(void){
+    return periode;
+}
+
+/*void setAudioPeriode(char nouPeriode){
+    periode[0] = nouPeriode;
+    periode[1] = nouPeriode+1;
+    periode[2] = nouPeriode+2;
+    periode[3] = nouPeriode+3;
+}*/
+
+void turnOffAudio(){
+    estat = 2;
+}
+
+char getAudioStatus(void){
+    return estat != 2;
+}
+
+void MotorAudio(){
+    switch(estat){
+        case 0:
+            if (TiGetTics(timerAudio) >= periode[frequencia]){
+                TiResetTics(timerAudio);
+                AUDIO_ON();
+                estat = 1;
+            }
+            break;
+        case 1:
+            if (TiGetTics(timerAudio) >= periode[frequencia]){
+                TiResetTics(timerAudio);
+                AUDIO_OFF();
+                estat = 0;
+            }
+            break;
+        case 2:
+            break;
+    }
+}
+
+
+char changeAudioStatus(){
+    //Post Canvia l'estat d'audio
+    if (estat == 2){
+        estat = 0;
+    }else{
+        estat = 2;
+        AUDIO_OFF();
+    }
+    return estat == 0;
+}
+
+void seguentFrequencia(){
+    if(frequencia == 3){
+        frequencia = 0;
+    }else{
+        frequencia++;
+    }
+}
+
+static unsigned char estatt, melodia, cantat;
+int  vanalog, vanalogantic, tempsConvertit;
+
+void AuControlInit(){
+    estatt = 1;
+    melodia = 0;
+    cantat=0;
+    frequencia=0;
+    vanalog=AdGetMostra(); 
+    tempsConvertit=(1000*binaryToDecimal(vanalog))/1024;
+            
+}
+
+void MotorControlAudio(){
+    switch(estatt){
+        case 0:
+            
+            if (TiGetTics(timerTemps) >= tempsConvertit){
+                if(melodia==0){
+                    seguentFrequencia();
+                }else if(melodia==1){
+                    seguentFrequencia();
+                    seguentFrequencia();
+                }else if(melodia==2){
+                    seguentFrequencia();
+                    seguentFrequencia();
+                    seguentFrequencia();
+                }
+                cantat++;
+                TiResetTics(timerTemps);
+                if(cantat == 3){
+                    estatt = 1;
+                    changeAudioStatus();
+                }else{
+                    estatt = 2;
+                }
+            }
+            break;
+            
+        case 1:
+
+            break;
+        
+        case 2:
+            vanalog=AdGetMostra();
+            if(vanalog != vanalogantic) {
+                tempsConvertit=(1000*binaryToDecimal(vanalog))/1024;
+                vanalogantic=vanalog;
+            }
+            estatt = 0;
+            
+            break;
+    }
+}
+
+void audioInicial(){
+    estatt=0;
+    melodia=0;
+    cantat=0;
+    TiResetTics(timerTemps);
+    changeAudioStatus();
+    frequencia=0;
+}
+
+void audioDurant(){
+    estatt=0;
+    melodia=1;
+    cantat=0;
+    TiResetTics(timerTemps);
+    changeAudioStatus();
+    frequencia=0;
+}
 
 void audioFinal(){
     estatt=0;
