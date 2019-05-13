7a4cc0cae4c56cd7ef21706dda1932cda7c98a39
Marc Llort <marc.llort@students.salle.url.edu>
2019-05-13 19:10:40 +0200

    FUNCIONA TEMP

--- Thermometer.h
+++ Thermometer.h
@@ -1,21 +1,22 @@
-//* 
- * File:   
- * Author: 
- * Comments:
- * Revision history: 
- */
-
-// This is a guard condition so that contents of this file are not included
-// more than once.  
 #ifndef Thermometer
 #define	Thermometer
 
 #include <xc.h> // include processor files - each processor file is guarded.  
+
+
+#define CLOCK TRISBbits.TRISB13
 #define T LATBbits.LATB13
-#define SERIE LATBbits.LATB14
 
-#define INOUT TRISBbits.TRISB14
+//#define INOUT TRISBbits.TRISB14
+//#define SERIE PORTBbits.RB14
+#define INOUT TRISAbits.TRISA2
+#define SERIE PORTAbits.RA2
 
+#define CSTRIS TRISAbits.TRISA3
+#define CS PORTAbits.RA3
+
+
+void ThermometerInit();
 void MotorThermometer(void);
 void llegeixBit();
 char* GetTemperature();
