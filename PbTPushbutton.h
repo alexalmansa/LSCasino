603d158977e196d2bea1358b43ef565f3f2e8637
alexalmansa <alexalmansa5@gmail.com>
2019-05-15 17:22:38 +0200

    Update PbTPushbutton.h

--- PbTPushbutton.h
+++ PbTPushbutton.h
@@ -7,32 +7,30 @@
  * Created on 27 de enero de 2014, 19:19
  */
 
+
 #ifndef PBTPUSHBUTTON_H
 #define	PBTPUSHBUTTON_H
 
-//Files del teclat
-#define F1 LATBbits.LATB6
-#define F2 LATBbits.LATB7
-#define F3 LATBbits.LATB8
-#define F4 LATBbits.LATB9
-
- //Columnes    
-#define C1 PORTBbits.PORTB10
-#define C2 PORTBbits.PORTB11
-#define C3 PORTBbits.PORTB12
-
-#define STEPBL  5 //Més val que sigui múltiple de BLPERIOD....
-
 #include<xc.h>
 #include "time.h"
-#include "BlTBacklight.h"
+
+//Files del teclat
+#define F4 LATBbits.LATB6
+#define F3 LATBbits.LATB7
+#define F2 LATBbits.LATB8
+#define F1 LATBbits.LATB9
+
+ //Columnes    
+#define C3 PORTBbits.RB10
+#define C2 PORTBbits.RB11
+#define C1 PORTBbits.RB12
+
+
+
+
 void PbInit(void);
 //Posa els ports d'entrada i demana un timer!
-char getPB1(void);
-//Post: retorna l'estat del pulsador - (0 o 1)
 
-char getPB2(void);
-//Post: retorna l'estat del pulsador + (0 o 1)
 
 void MotorPulsador();
 
