97d67bc1336b259a95ccc5ca08e213018f872d69
alexalmansa <alexalmansa5@gmail.com>
2019-05-16 13:42:20 +0200

    Update PWM.c

--- PWM.c
+++ PWM.c
@@ -12,7 +12,7 @@ static char timerPWM, temps;
 static char start = 0;
 
 static int TEMPSA1= 1, FREQ = 20, countPWM=0,casella;
-static double FLANCSXCASELLA = 1.46 , restants,flancs = 263;
+static double FLANCSXCASELLA = 1.44 , restants,flancs = 263;
 static int posicions[36] = {0,14,31,2,33,18,27,6,21,10,19,23,4, 25,12,35,16,29,8 ,34,13,32,9 ,20,17,30,1 ,26,5 ,7 ,22,11,36,15,28,3 ,24};
 //                          0,1 ,2 ,3 ,4, 5, 6,7, 8, 9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36 
 
@@ -41,7 +41,7 @@ static int posicions[36] = {0,14,31,2,33,18,27,6,21,10,19,23,4, 25,12,35,16,29,8
       
       casella = posicions[casellac] ;
       flancs = casella * FLANCSXCASELLA + flancs;
-      restants = 52.6 - casella; 
+      restants = 51.8 - casella; 
       PWMOn();
     }
     void PWMOn(){
@@ -70,7 +70,7 @@ static int posicions[36] = {0,14,31,2,33,18,27,6,21,10,19,23,4, 25,12,35,16,29,8
                     }
                     if (countPWM >= /*263*/flancs){
                         countPWM = 0;
-                        flancs = 263 + restants;
+                        flancs = 259 + restants;
                         PWMOff();
                         
                     }
